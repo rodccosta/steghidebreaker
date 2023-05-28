e3474ea0ae71ee72e7df0f079a81d7521f4943cb
Rodrigo Costa <rodccosta@hotmail.com>
2023-05-28 09:21:27 -0300

    Update steghidebreaker.sh
    include guide to use

--- steghidebreaker.sh
+++ steghidebreaker.sh
@@ -1,5 +1,17 @@
 #author: rodccosta
 #/bin/bash
+len=$#
+if [ $len -eq 1 ]
+then
+        echo -e "\033[31mDicionário não informado \033[0m"
+fi
+if [ $len -lt 2 ]
+then
+        echo -e "\033[31mNúmero de parâmetros inválido \033[0m"
+        echo "Forma de uso:"
+        echo "./steghidebreaker.sh nome_do_arquivo dicionario.txt"
+        exit 0
+fi
 cat $2 | while read password
 do
 	result=$(steghide extract -sf $1 -p $password 2>&1 | grep "wrote" | wc -l )
